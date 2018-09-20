f33cbae10bd7d088068c3901126690c2c41ae18a
Throgar <jakub.lehotsky@gmail.com>
2018-09-20 18:35:59 +0200

    Camera setup and basic ui

--- app/src/main/java/com/example/jakub/numberrecognizer/MainActivity.java
+++ app/src/main/java/com/example/jakub/numberrecognizer/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.jakub.numberrecognizer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout frameLayout = findViewById(R.id.frameLayout);
+
+        if (checkCameraHardware(this)) {
+            camera = getCameraInstance();
+        } else {
+            Log.e("ERROR", "Camera is not available");
+        }
+
+        ShowCamera surface = new ShowCamera(this, camera);
+
+        frameLayout.addView(surface);
+
+    }
+
+    private boolean checkCameraHardware(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return c;
     }
 }

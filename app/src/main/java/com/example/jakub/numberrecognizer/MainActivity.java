f2cb6a9e4420a4283b3e189c0ae8f688088ccbe4
Throgar <jakub.lehotsky@gmail.com>
2018-09-21 18:33:18 +0200

    Functional app.

--- app/src/main/java/com/example/jakub/numberrecognizer/MainActivity.java
+++ app/src/main/java/com/example/jakub/numberrecognizer/MainActivity.java
@@ -1,33 +1,43 @@
 package com.example.jakub.numberrecognizer;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.FrameLayout;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Camera camera;
+    CameraSource cameraSource;
+    SurfaceView surfaceView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FrameLayout frameLayout = findViewById(R.id.frameLayout);
+        surfaceView = findViewById(R.id.surfaceView);
+        textView = findViewById(R.id.recognized);
 
         if (checkCameraHardware(this)) {
-            camera = getCameraInstance();
+            startCameraSource();
         } else {
             Log.e("ERROR", "Camera is not available");
         }
-
-        ShowCamera surface = new ShowCamera(this, camera);
-
-        frameLayout.addView(surface);
 
     }
 
@@ -35,14 +45,76 @@ public class MainActivity extends AppCompatActivity {
         return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
 
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open();
+
+    private void startCameraSource() {
+
+        final TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        if (!textRecognizer.isOperational()) {
+            Log.w("MainActivity", "Detector dependencies not loaded yet");
+        } else {
+
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setAutoFocusEnabled(true)
+                    .setRequestedFps(2.0f)
+                    .build();
+
+            surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder holder) {
+                    try {
+
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    101);
+                            return;
+                        }
+                        cameraSource.start(surfaceView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder holder) {
+                    cameraSource.stop();
+                }
+            });
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if (items.size() != 0 ){
+
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for(int i=0;i<items.size();i++){
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+                                }
+                                textView.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
         }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return c;
     }
 }
--- app/src/main/java/com/example/jakub/numberrecognizer/ShowCamera.java
+++ app/src/main/java/com/example/jakub/numberrecognizer/ShowCamera.java
@@ -1,32 +1,60 @@
 package com.example.jakub.numberrecognizer;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
-import android.content.res.Configuration;
-import android.hardware.Camera;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.IOException;
 
 public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
 
-    Camera camera;
+//    Camera camera;
     SurfaceHolder holder;
     Context context;
+    CameraSource cameraSource;
+    TextRecognizer textRecognizer;
 
-    ShowCamera(Context context, Camera camera) {
+    /*ShowCamera(Context context){
         super(context);
         this.context = context;
-        this.camera = camera;
+    }*/
+
+    ShowCamera(final Context context/*, Camera camera*/) {
+        super(context);
+        this.context = context;
+//        this.camera = camera;
         this.holder = getHolder();
         holder.addCallback(this);
+
+        textRecognizer = createCameraSource();
+
+
+
     }
 
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        Camera.Parameters params = camera.getParameters();
+//        Camera.Parameters params = camera.getParameters();
 
+        if(ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.CAMERA}, 101);
+        }
+/*
         if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             params.set("orientation", "portrait");
             camera.setDisplayOrientation(90);
@@ -38,12 +66,73 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
         }
 
         camera.setParameters(params);
+*/
         try {
-            camera.setPreviewDisplay(holder);
-            camera.startPreview();
+            //camera.setPreviewDisplay(holder);
+            cameraSource.start(holder);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public TextRecognizer createCameraSource(){
+        final TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
+
+
+
+        if(textRecognizer.isOperational()) {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            ((Activity)context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+            cameraSource = new CameraSource.Builder(context, textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(displayMetrics.widthPixels, displayMetrics.heightPixels)
+                    .setAutoFocusEnabled(true)
+                    .setRequestedFps(30.0f)
+                    .build();
+        } else {
+            Toast.makeText(context,"Detector dependencies not loaded yet.", Toast.LENGTH_LONG).show();
+            Log.w("ShowCamera", "Detector dependencies not loaded yet.");
+        }
+        return textRecognizer;
+    }
+
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                final TextView textView = findViewById(R.id.recognized);
+                final SparseArray<TextBlock> items = detections.getDetectedItems();
+
+                if(textView == null){
+                    Toast.makeText(context,"null textview", Toast.LENGTH_SHORT).show();
+                }
+
+                if(items.size() > 0) {
+
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for(int i = 0; i < items.size(); i++){
+                                TextBlock block = items.valueAt(i);
+                                stringBuilder.append(block);
+                            }
+                            textView.setText(stringBuilder);
+                        }
+                    });
+                }
+            }
+        });
 
 
     }
@@ -55,6 +144,8 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+        cameraSource.stop();
     }
+
+
 }
